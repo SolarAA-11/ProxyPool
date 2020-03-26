7acbe226649bd8581f758140be550bd6ad8b9c8b
SolarDomo <948872263@qq.com>
2020-03-26 14:47:54 +0800

    将 ProxyPool 单独分离出来

--- src/ProxyPool/main.py
+++ src/ProxyPool/main.py
@@ -1,16 +1,10 @@
 import asyncio, logging, configparser
 
-import uvicorn
-from fastapi import FastAPI
-
 from ProxyPool import ProxyPool ,create_proxypool
 from ProxyPool.models import ProxyItem
 
-app = FastAPI()
-proxy_pool: ProxyPool = None
 
-@app.on_event("startup")
-def on_app_startup():
+async def main():
     # 读取配置
     config = configparser.ConfigParser()
     # 默认配置文件
@@ -19,7 +13,6 @@ def on_app_startup():
     config.read("./production/config/production.cfg", encoding="UTF-8")
 
     # 启动代理池
-    global proxy_pool
     proxy_pool = create_proxypool(
         crawl_job_interval_hour=config.getint("ProxyPool", "crawl_job_interval_hour"),
         validate_job_interval_minute=config.getint("ProxyPool", "validate_job_interval_minute"),
@@ -28,10 +21,9 @@ def on_app_startup():
         max_concurrent_request=config.getint("ProxyPool", "max_concurrent_request"),
         crawl_page_count_for_xici=config.getint("CrawlJobFactory", "xicidaili_page_count")
     )
-
-@app.get("/proxy", response_model=ProxyItem)
-def get_proxy():
-    return proxy_pool.get()
+    
+    # 等待两个生产协程对应的 Task
+    await proxy_pool.task_for_produce_crawl_validate_job
 
 if __name__ == "__main__":
     # asyncio.run(main())
